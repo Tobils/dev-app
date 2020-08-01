13268e7434a9e7218a97a81f4c6060c035c4ae68
ade suhada <ade.suhada@mail.ugm.ac.id>
2020-08-01 07:44:58 +0700

    finish food delivery app

--- food_delivery_ui/lib/screens/cart_screen.dart
+++ food_delivery_ui/lib/screens/cart_screen.dart
@@ -113,20 +113,87 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    currentUser.cart.forEach(
+      (Order order) => totalPrice += order.quantity * order.food.price,
+    );
+
     return Scaffold(
       appBar: AppBar(
           title: Center(
         child: Text('Cart (${currentUser.cart.length})'),
       )),
       body: ListView.separated(
-        itemCount: currentUser.cart.length,
+        itemCount: currentUser.cart.length + 1,
         itemBuilder: (BuildContext context, int index) {
-          Order order = currentUser.cart[index];
-          return _buildCartItem(order);
+          if (index < currentUser.cart.length) {
+            Order order = currentUser.cart[index];
+            return _buildCartItem(order);
+          }
+          return Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Estimasi waktu pengantaran :',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w600)),
+                    Text('25 min',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w600))
+                  ],
+                ),
+                SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Total Biaya :',
+                        style: TextStyle(
+                            fontSize: 20.0, fontWeight: FontWeight.w600)),
+                    Text('\$${totalPrice.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            color: Colors.green[700],
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w600))
+                  ],
+                ),
+                SizedBox(
+                  height: 90,
+                ),
+              ],
+            ),
+          );
         },
         separatorBuilder: (BuildContext context, int index) {
           return Divider(height: 1.0, color: Colors.grey);
         },
+      ),
+      bottomSheet: Container(
+        height: 100,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black26, offset: Offset(0, -1), blurRadius: 6.0)
+          ],
+        ),
+        child: Center(
+          child: FlatButton(
+            onPressed: () {},
+            child: Text(
+              'CHECKOUT',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
